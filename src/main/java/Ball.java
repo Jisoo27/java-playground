a79d7e3fd41496640424c5557311a3bd0c692489
jisu <jiiisu0227@gmail.com>
2021-12-01 02:13:25 +0900

    ✅ Add the strike test

--- src/main/java/Ball.java
+++ src/main/java/Ball.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Ball {
     private final int position; // 로컬변수 할당
     private final int ballNo;
@@ -8,9 +10,30 @@ public class Ball {
     }
 
     public BallStatus play(Ball ball) {
-        if(ballNo == ball.ballNo) {
+        if(this.equals(ball)) {
+            return BallStatus.STRIKE;
+        }
+
+        if (ball.matchBallNo(ballNo)) {
             return  BallStatus.BALL;
         }
         return BallStatus.NOTHING;
+    }
+
+    private boolean matchBallNo(int ballNo) { // 해당하는 객체에 메세지 보내기(?)
+        return this.ballNo == ballNo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        return position == ball.position && ballNo == ball.ballNo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, ballNo);
     }
 }
--- src/test/java/BallTest.java
+++ src/test/java/BallTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -5,9 +6,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BallTest {
 
+    private Ball com;
+
+    @BeforeEach // 본 어노테이션을 붙인 메서드는 테스트 메서드 이전에 수행된다.
+    void setUp() { // 이렇게 공통된 부분을 분리하여 적어주면 각 메서드에서 중복으로 발생하는 부분을 제거할 수 있다.
+        com = new Ball(1, 4);
+    }
+
+    @Test
+    void strike() {
+        BallStatus status = com.play(new Ball(1, 4));
+        assertThat(status).isEqualTo(BallStatus.STRIKE);
+    }
+
+
     @Test
     void ball() {
-        Ball com = new Ball(1, 4);
         BallStatus status = com.play(new Ball(2, 4));
         assertThat(status).isEqualTo(BallStatus.BALL);
     }
@@ -15,7 +29,6 @@ public class BallTest {
 
     @Test
     void nothing() {
-        Ball com = new Ball(1, 4);
         BallStatus status = com.play(new Ball(2, 5));
         assertThat(status).isEqualTo(BallStatus.NOTHING);
     }
