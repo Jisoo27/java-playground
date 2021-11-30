87db65985ded395b40eb2cd86e751a0371df6f3d
jisu <jiiisu0227@gmail.com>
2021-12-01 03:37:35 +0900

    ✅ add the Remove duplicates

--- src/test/java/BallTest.java
+++ src/test/java/BallTest.java
@@ -1,19 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BallTest {
 
+    private Ball com;
+
+    @BeforeEach
+    void setUp() {
+        com = new Ball(1,4);
+    }
+
     @Test
     void Ball() {
-        Ball com = new Ball(1, 4);
         BallStatus status = com.play(new Ball(2 ,4));
         assertThat(status).isEqualTo(BallStatus.BALL);
     }
 
     @Test
     void nothing() {
-        Ball com = new Ball(1, 4);
         BallStatus status = com.play(new Ball(2 ,5));
         assertThat(status).isEqualTo(BallStatus.NOTHING);
     }
