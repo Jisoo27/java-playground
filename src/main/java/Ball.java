77fff8c80a1ced92da71d6af91cdcdf5d3c08d2b
jisu <jiiisu0227@gmail.com>
2021-12-01 03:42:15 +0900

    ✅ add the strike test

--- src/main/java/Ball.java
+++ src/main/java/Ball.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Ball {
     private final int position;
     private final int ballNo;
@@ -8,9 +10,29 @@ public class Ball {
     }
 
     public BallStatus play(Ball ball) {
-        if(ballNo == ball.ballNo) {
+        if (this.equals(ball)) {
+            return BallStatus.STRIKE;
+        }
+        if (ball.matchBallNo(ballNo)) {
             return BallStatus.BALL;
         }
         return BallStatus.NOTHING;
+    }
+
+    private boolean matchBallNo(int ballNo) {
+        return this.ballNo == ballNo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        return position == ball.position && ballNo == ball.ballNo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, ballNo);
     }
 }
--- src/test/java/BallTest.java
+++ src/test/java/BallTest.java
@@ -13,7 +13,13 @@ public class BallTest {
     }
 
     @Test
-    void Ball() {
+    void strike() {
+        BallStatus status = com.play(new Ball(1 ,4));
+        assertThat(status).isEqualTo(BallStatus.STRIKE);
+    }
+
+    @Test
+    void ball() {
         BallStatus status = com.play(new Ball(2 ,4));
         assertThat(status).isEqualTo(BallStatus.BALL);
     }
