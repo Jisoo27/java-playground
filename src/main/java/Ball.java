ac04e143a168ca78bba5747567e62f94492098a5
jisu <jiiisu0227@gmail.com>
2021-12-01 03:35:57 +0900

    ✅ add the Ball test

--- src/main/java/Ball.java
+++ src/main/java/Ball.java
@@ -1,9 +1,16 @@
 public class Ball {
-    public Ball(int i, int i1) {
+    private final int position;
+    private final int ballNo;
 
+    public Ball(int position, int ballNo) { // constructor 함수
+        this.position = position; // 값 할당
+        this.ballNo = ballNo;
     }
 
     public BallStatus play(Ball ball) {
+        if(ballNo == ball.ballNo) {
+            return BallStatus.BALL;
+        }
         return BallStatus.NOTHING;
     }
 }
--- src/test/java/BallTest.java
+++ src/test/java/BallTest.java
@@ -5,6 +5,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BallTest {
 
     @Test
+    void Ball() {
+        Ball com = new Ball(1, 4);
+        BallStatus status = com.play(new Ball(2 ,4));
+        assertThat(status).isEqualTo(BallStatus.BALL);
+    }
+
+    @Test
     void nothing() {
         Ball com = new Ball(1, 4);
         BallStatus status = com.play(new Ball(2 ,5));
