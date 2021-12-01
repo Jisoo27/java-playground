46003199a09e0820ee4acf3db94d66ab962cc41f
jisu <jiiisu0227@gmail.com>
2021-12-02 00:07:51 +0900

    DOCS: 캡슐화 예제

---
+++ src/main/java/EncapsulationClass.java
@@ -0,0 +1,23 @@
+public class EncapsulationClass {
+    private int id; // private 접근 제어자를 통해 변수를 선언해 주었기 때문에 외부에서 접근 할 수 없다.
+    private String name;
+
+    // 그래서 getter / setter 메소드를 통해 변수에 값을 저장해주고 가져 올 수 있게 해준다.
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        EncapsulationClass cap = new EncapsulationClass();
+
+        cap.setId(123);
+        cap.setName("jay");
+
+        System.out.println(cap.getId());
+        System.out.println(cap.getName());
+    }
+}
